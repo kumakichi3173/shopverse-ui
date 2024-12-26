a04d3145d1b365ee53d0e52c0b7514af326b3889
Saori Shigehisa <saorishigehisa@Saoris-MacBook-Air.local>
2024-12-25 19:06:30 -0700

    created login, signup, home, and menu pages

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024 - Shiny Superb Star
+ * All rights reserved.
+ *
+ * This file is the property of Shiny Superb Star.
+ * Unauthorized use, copying, or distribution is strictly prohibited.
+ * Shiny Superb Star reserves the right to take legal action
+ * against any person or entity who makes use of this file without the
+ * express written permission of Shiny Superb Star.
+ * 
+ */
+
+import React, {useState} from "react";
+
+const ItemCounter = () => {
+    const [count, setCount] = useState(initialState: 0)
+
+    const countUp = () => {
+        setCount( value: prevState => prevState +1)
+    }
+
+    const countDown = () => {
+        setCount( value: prevState => prevState -1)
+    }
+}
+
+return (
+
+    
+)
