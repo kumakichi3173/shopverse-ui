e060f92b2cbffdef7ebb91a02aba63fb5f7898ce
Saori Shigehisa <saorishigehisa@Saoris-MacBook-Air.local>
2025-01-01 18:33:09 -0700

    resoled PR comment

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -20,7 +20,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import { Button } from '@/components/ui/button'
 import { Input } from "@/components/ui/input";
 
-export default function Signup() {
+export default function SignUp() {
   const {
     register,
     handleSubmit,
