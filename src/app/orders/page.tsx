ac52004c3635fa364d9914fab5dfe9a6657e6eda
Saori Shigehisa <saorishigehisa@Saoris-MacBook-Air.local>
2025-01-12 22:41:06 -0700

    order page was created

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 - Shiny Superb Star
+ * All rights reserved.
+ *
+ * This file is the property of Shiny Superb Star.
+ * Unauthorized use, copying, or distribution is strictly prohibited.
+ * Shiny Superb Star reserves the right to take legal action
+ * against any person or entity who makes use of this file without the
+ * express written permission of Shiny Superb Star.
+ * 
+ */
+
+"use client"
+
+import styles from './styles.module.css'
+import * as React from "react"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+export default function Orders() {
+    return (
+        <>
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href="/docs/components">Your account</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>Your orders</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <h1 className={styles.h1}>Your orders</h1>
+
+            <div className="flex items-center">
+                <div>
+                    <div className="flex w-full max-w-sm items-center">
+                        <Input type="searchallorders" placeholder="Search all orders" />
+                        <Button about='Search all orders' aria-label='Search all orders' variant='default'>
+                            <Search height={200} width={200} />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
